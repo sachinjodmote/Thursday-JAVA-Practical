d5beafa3d64a69a2f85d16cf865b70f73e20f274
Sachin Jodmote <111167433+sachinjodmote@users.noreply.github.com>
2023-03-24 10:38:24 +0530

    Create calc.java

---
+++ 23-03-2023/calc.java
@@ -0,0 +1,41 @@
+// Write a program create a class calsculation add methos addition,substraction,multiplication,and division to it create objects and access those methods.
+class Calculation  //declaring class
+{
+   int a;   //   variable initialization                        
+   int b;   //   variable initialization
+   int result; // variable initialization
+   
+   void addition(int a, int b)    //adition method
+   {
+      result=a+b;  // performing addition
+	  System.out.println("Addition:"+result);  // print result
+   }
+   void substraction(int a, int b)  // substraction method
+   {
+      result=a-b; //performing substraction
+	  System.out.println("Substraction:"+result);  // print result
+   }
+   void multiplicaction(int a, int b)  // multiplication method
+   {
+      result=a*b;  // performing multiplication
+      System.out.println("multiplicaction:"+result);	  // print result
+   }
+   void division(int a, int b)   // division method
+   {
+       result=a/b;  // performing division
+       System.out.println("division:"+result);// print result
+    }
+
+}
+class SumCalculation   //declaring class
+{
+    public static void main(String args[])  // main method
+    {
+       Calculation sum=new Calculation();  // creating object
+        sum.addition(10,20);          //marameter passing
+        sum.substraction(30,20);      //marameter passing
+        sum.multiplicaction(2,5);     //marameter passing
+        sum.division(2,2);	      //marameter passing
+    }
+	
+}
