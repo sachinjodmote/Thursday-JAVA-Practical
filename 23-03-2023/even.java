807b44f1941a8da1b889ee9558dc154eed866c6a
Sachin Jodmote <111167433+sachinjodmote@users.noreply.github.com>
2023-03-24 10:39:30 +0530

    Create even.java

---
+++ 23-03-2023/even.java
@@ -0,0 +1,18 @@
+// Write a java program to find if the number is odd or even
+
+class Even  //class declaration
+{
+	 public static void main(String args[])   //main method
+	 {
+		  int a = 33; // value initialization
+		  
+		  if(a%2==0)    // if condition declaration
+		  {
+			  System.out.println("The number is even");    // condition statement
+		  }
+		  else     // else condition declaration
+		  {
+			  System.out.println("The number is odd");    // condition statement
+		  }
+	 }
+}
